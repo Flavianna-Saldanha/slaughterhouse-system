80d32a270f17e9ee10fc6c7ce9aa30b28ad0802f
Flavianna Saldanha <110172991+Flavianna-Saldanha@users.noreply.github.com>
2026-03-16 19:41:21 +0000

    add `database.js` and `.env` files

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -107,7 +107,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "65fa9b4cfa9c94fb"; }
+/******/ 		__webpack_require__.h = function() { return "e8d2552543bcd938"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
